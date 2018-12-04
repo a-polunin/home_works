9e74a58ffaf02f626d00d0f12e9bd825ccf49dde
bazikk <33804374+bazikk@users.noreply.github.com>
2018-12-05 00:45:01 +0300

    fix

--- index.js
+++
@@ -1,9 +0,0 @@
-const data = require('./home-work').data
-const parser = require('./home-work').parser
-const fs = require('fs')
-const htmlFileText = fs.readFileSync('./home-work/index.html','utf-8')
-
-
-console.log(parser(data))
-console.log('**********')
-console.log(htmlFileText)
